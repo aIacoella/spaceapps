7930bd8084b175575cf840c7b65a12f6eef70f03
ihategoto <cdemilio80@gmail.com>
2018-10-21 00:45:53 +0200

    ecereù

--- img/updater.php
+++ img/updater.php
@@ -2,15 +2,17 @@
     include_once "../php/db_connection.php";
     $images=array_slice(scandir("."), 2);
     for($i = 0; $i < count($images); $i++){
-        if(($r = $conn->query("SELECT * FROM images WHERE path = '".$conn->real_escape_string($images[$i])."'"))){
-            if($r->num_rows == 0){
-                if(!$conn->query("INSERT INTO images (path, tag) VALUES('".$conn->real_escape_string($images[$i])."',1)")){
-                    echo $conn->error."<br>";
-                    exit();
+        if(strcmp($images[$i], "updater.php") !== 0){
+            if(($r = $conn->query("SELECT * FROM images WHERE path = '".$conn->real_escape_string($images[$i])."'"))){
+                if($r->num_rows == 0){
+                    if(!$conn->query("INSERT INTO images (path, tag) VALUES('".$conn->real_escape_string($images[$i])."',1)")){
+                        echo $conn->error."<br>";
+                        exit();
+                    }
                 }
+            }else{
+                echo $conn->error."<br>";
             }
-        }else{
-            echo $conn->error."<br>";
         }
     }
     $conn->close();
--- js/game.js
+++ js/game.js
@@ -1,9 +1,6 @@
 
 Window.onload=function (){
-    $.ajax({
-        url: '../php/shuffle.php',
-        dataType: 'json',
-        success: function(response) {
-            console.log(response);
-            }
-        })};
+    $.get( "../php/shuffle.php", function( data ) {
+        console.log(data);
+      });
+    };
