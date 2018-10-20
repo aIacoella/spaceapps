fba8901ecff73c0789d7a827892d1b828a8ee19c
aIacoella <angeloiacoella3@gmail.com>
2018-10-20 12:22:14 +0200

    push

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
 </head>
 <body>
-    
+    <div class="">
+    </div>
 </body>
 </html>
