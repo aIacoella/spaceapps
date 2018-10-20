4f69540e6606ee188b96640ca5dc912423fdd3cd
ihategoto <cdemilio80@gmail.com>
2018-10-20 16:00:41 +0200

    Aggiunto: updater.php

---
+++ img/updater.php
@@ -0,0 +1,11 @@
+<?php
+    include_once "../db/db_connection.php";
+    $images=array_slice(scandir("."), 2);
+    for($i = 0; $i < count($images); $i++){
+        if(($conn->query("SELECT * FROM images WHERE path = $images[$i]"))->num_rows == 0){
+            if(!$conn->query("INSERT INTO images (path) VALUES(".$conn->real_escape_string($images[$i]).")")){
+                exit();
+            }
+        }
+    }
+?>
