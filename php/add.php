ebfaca64513a762a0b6d1ce44d64af85f6378d03
ihategoto <cdemilio80@gmail.com>
2018-10-21 01:26:42 +0200

    img

---
+++ php/add.php
@@ -0,0 +1,14 @@
+<?php
+    if(!isset($_POST['name']) || !isset($_POST['content'])){
+        /*
+        Errore richiesta
+        */
+    }else{
+        include_once "db_connection.php";
+        $name = $conn->real_escape_string($_POST['name']);
+        $content = $conn->real_escape_string($_POST['content']);
+
+        $query = "INSERT INTO comments (content, img) SELECT $content, img_id FROM images WHERE path = '$name'";
+
+    }
+?>
