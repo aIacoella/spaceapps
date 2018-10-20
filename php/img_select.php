4902074c369a01439e7704097d0365796f70a91b
ihategoto <cdemilio80@gmail.com>
2018-10-20 19:08:18 +0200

    ciao

--- php/img_select.php
+++ php/img_select.php
@@ -1,26 +1,28 @@
 <?php
+    $reply = new stdClass();
+    include_once "db_connection.php";
 
-    if(!isset($_POST['tag'])){
-        echo "";
+    if(!isset($_GET['tag'])){
+        echo "Ciao";
     }else{
         /* Select all images which have the chosen tag */
         include_once "db_connection.php";
-        $chosen_tag = $conn->real_escape_string($_POST['tag']);
-        $query = "SELECT path FROM images INNER JOIN tags ON tags.name = $chosen_tag WHERE tags.name =".$chosen_tag;
+        $chosen_tag = $conn->real_escape_string($_GET['tag']);
+        $query = "SELECT path, tags.tag_id FROM images INNER JOIN tags ON tags.name = '".$chosen_tag."' WHERE images.tag = tags.tag_id ";
         
 
         if(!($r=$conn->query($query))){
             echo "<p> Errore nel database</p>";
         }else{
             $i = 0;
-            $reply;
             while(($row = $r->fetch_array(MYSQLI_ASSOC))){
-               $reply->path{"$i"} = $row['path'];
+               echo $reply->tag{"$i"}=$row['path']."<br>";
+               $i++;
             }
-        }
         $r->close();
-        $mysqli->close();
-        echo json_encode($reply);
+        }
+        $conn->close();
+
         exit();
     }
 ?>
