7aeb7bd78dd5696cc1477676677821bb2003527d
aIacoella <angeloiacoella3@gmail.com>
2018-10-21 07:46:48 +0200

    ao

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var score = 1;
+var score = 0;
 var correct_answer = -1;
 
 window.onload=function (){
@@ -6,20 +6,34 @@ window.onload=function (){
 };
 
 function updateGame(){
+  score++;
+  document.getElementById("current-score").innerText = score;
   $.get( "../php/shuffle.php", function( data ) {
     console.log(data);
     data = JSON.parse(data);
     console.log(data[0]);
     document.getElementById("mainImg").src = "../img/"+data[0];
-    document.getElementById("answer1").innerHTML = data[1];
-    document.getElementById("answer2").innerHTML = data[2];
-    document.getElementById("answer3").innerHTML = data[3];
-    document.getElementById("answer4").innerHTML = data[4];
+    indexes = [1, 2, 3, 4];
+    for(let i=1; i<=4; i++){
+      let index = Math.floor(Math.random()*indexes.length) + 1;
+      document.getElementById("answer" + i).innerText = data[index];
+      if(index === 1){
+        correct_answer = i;
+      }
+      indexes.splice(index, 1);
+    }
   });
 }
 
 function answer(answer){
-  if(answer==correct_answer){
-
+  if(answer!==correct_answer){
+    $.ajax({
+      type: 'post',
+      url: '../php/leaderboard.php',
+      score: score-1,
+      dataType: 'json',
+    });
+  } else {
+    updateGame();
   }
 }
