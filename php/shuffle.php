20e0b8a4e8b747f6429cec42cf3507643a4c09ba
ihategoto <cdemilio80@gmail.com>
2018-10-31 10:49:51 +0100

    Added: Handling process based around sessions system.

--- php/shuffle.php
+++ php/shuffle.php
@@ -5,31 +5,64 @@
 
         It returns the data needed to run the game.
     */
-    if(!isset($_COOKIE['game_image']) && !isset($_COOKIE['game_session'])){
+    if(!isset($_SESSION['game_session'])){
         include_once "db_connection.php";
-        $query = "SELECT comment_id, images.path, content FROM comments INNER JOIN images ON img_id = img ORDER BY rand() limit 1";
+        session_start();
+        $query = "SELECT img, images.path, content FROM comments INNER JOIN images ON img_id = img ORDER BY rand() limit 1";
         if(!($r=$conn->query($query))){
             echo $conn->close();
             $conn->close();
             die();
         }
-        /* It's the first call from the client */
-        mt_srand(time());
-        $session = mt_rand(1000, 1000000);
-        if(($fd = fopen($session, 'w')) === FALSE){
-            echo "<span style = \"display:block\">Internal error</span>";
+        $row = $r->fetch_array(MYSQLI_ASSOC);
+        $session = array_merge($session, $row['path']);
+        $_SESSION['game_session'] = $session;
+        $reply = array();
+        $reply[0] = $row['path'];
+        $reply[1] = htmlspecialchars($row['content'], ENT_COMPAT, 'UTF-8');
+        $r->close();
+        $query = "SELECT content FROM comments WHERE img !=". $row['img']." ORDER BY rand() limit 3";
+        if(!($r = $conn->query($query))){
+            echo $conn->error;
+            $conn->close();
+            die();
+        }
+        $i = 2;
+        while(($row = $r->fetch_array(MYSQLI_ASSOC))){
+            $reply[$i] = htmlspecialchars($row['content'], ENT_COMPAT, 'UTF-8');
+            $i++;
+        }
+        $r->close();
+        $conn->close();
+        echo json_encode($reply);
+        die();
+    }else if(isset($_SESSION['game_session'])){
+        include_once "db_connection.php";
+        /* Session is an array */
+        $session = array();
+        $session = $_SESSION['game_session'];
+        $query = "SELECT img, images.path, content FROM comments INNER JOIN images ON img_id = img WHERE images.path NOT IN (".get_images($session).") ORDER BY rand() limit 1";
+        if(!($r=$conn->query($query))){
+            echo $conn->close();
+            $conn->close();
+            die();
+        }
+        if($r->num_rows === 0){
+            echo "<span style=\"display:block;\">You won.</span><span> If you want more poetry, just write it by yourself!</span>";
+            /* Session is aborted here: remove it from leaderboard.php*/
+            session_abort();
             $r->close();
             $conn->close();
             die();
         }
-        $row = $r->fetch_array(MYSQLI_ASSOC);
-        setcookie('game_session', $session, 0, '/');
-        setcookie('game_image', $row['path'], 0, '/');
         $reply = array();
+        $row = $r->fetch_array(MYSQLI_ASSOC);
+        $session = array_merge($session, $row['path']);
+        $_SESSION['game_session'] = $session;
         $reply[0] = $row['path'];
         $reply[1] = htmlspecialchars($row['content'], ENT_COMPAT, 'UTF-8');
         $r->close();
-        $query = "SELECT content FROM comments WHERE comment_id !=". $row['comment_id']." ORDER BY rand() limit 3";
+        $query = "SELECT content FROM comments WHERE img !=". $row['img']." ORDER BY rand() limit 3";
         if(!($r = $conn->query($query))){
             echo $conn->error;
             $conn->close();
@@ -42,73 +75,18 @@
         }
         $r->close();
         $conn->close();
-        fclose($fd);
         echo json_encode($reply);
-        die();
-    }else if(isset($_COOKIE['game_image']) && isset($_COOKIE['game_session'])){
-        include_once "db_connection.php";
-        $session = $_COOKIE['game_session'];
-        $image = $_COOKIE['game_image'];
-        if(($fd = fopen($session, 'a+')) === FALSE){
-            echo "Internal error";
-            die();
-        }
-        if(fwrite($fd, $image."\n") === FALSE){
-            echo "Internal error";
-            $conn->close();
-            fclose($fd);
-            die();
-        }
-        rewind($fd);
-        $query = "SELECT comment_id, images.path, content FROM comments INNER JOIN images ON img_id = img WHERE images.path NOT IN (".get_images($fd).") ORDER BY rand() limit 1";
-        if(!($r = $conn->query($query))){
-            echo $conn->error.$query."\n";
-            fclose($fd);
-            $conn->close();
-            die();
-        }
-        $reply = array();
-        $row = $r->fetch_array(MYSQLI_ASSOC);
-        setcookie('game_image', $row['path'], 0, '/');
-        $reply[0] = $row['path'];
-        $reply[1] = htmlspecialchars($row['content'], ENT_COMPAT, 'UTF-8');
-        $r->close();
-        $query = "SELECT content FROM comments WHERE comment_id !=". $row['comment_id']." ORDER BY rand() limit 3";
-        if(!($r = $conn->query($query))){
-            echo $conn->error;
-            $conn->close();
-            fclose($fd);
-        }
-        $i = 2;
-        while(($row = $r->fetch_array(MYSQLI_ASSOC))){
-            $reply[$i] = htmlspecialchars($row['content'], ENT_COMPAT, 'UTF-8');
-            $i++;
-        }
-        fclose($fd);
-        $r->close();
-        $conn->close();
-        echo json_encode($reply);
-        die();
-    }else{
-        echo "Security: Something went wrong!";
         die();
     }
 
-    function get_images($session_file = NULL){
-        if($session_file == NULL)
+    function get_images($session = NULL){
+        if($session == NULL || !is_array($session))
             return false;
         $images_a = "";
-        $read;
-        $i = 1;
-        while(($read = fgets($session_file, 4096)) !== false ){
-            $read = substr($read, 0, strlen($read)-1);
-            $read = "'".$read."'";
-            if($i != 1)
-                $images_a .= ",".$read;
-            else    
-                $images_a .= $read;
-            $i++;
+        for($i = 0; $i < count($session); $i++){
+            $images_a .= $session[$i].',';
         }
+        $images_a[$i-1] = "";
         return $images_a;
     }
 ?>
